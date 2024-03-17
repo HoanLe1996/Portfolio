c9d1bf69be2e5c154ccd8623d74a90b325116ae6
HoanLV <tuibui1111@gmail.com>
2024-03-17 23:33:58 +0700

    fix: change content file

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend developer", "backend developer","android development", "web development"],
+    strings: ["frontend developer", "backend developer","android developer", "web developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
